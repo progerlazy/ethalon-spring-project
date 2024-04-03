e05e5250699fbbf964c7ac56400c537b178f6e91
Lazy Proger <progerlazy@gmail.com>
2024-04-03 13:53:01 +0300

    Init spring config

---
+++ src/main/java/com/progerlazy/ethalonspring/EthalonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.progerlazy.ethalonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthalonSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthalonSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progerlazy/ethalonspring/EthalonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progerlazy.ethalonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthalonSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
